5bf1f6b29ad1201259ba466f3d4fd50cd137a5c6
jsjst <tmfwlchlrh3@naver.com>
2024-07-02 18:06:54 +0900

    [전슬지] commit
    application 종료 시 JDBC Connection 해제

---
+++ src/main/java/com/salady/saladyJSP/MyAppContextListener.java
@@ -0,0 +1,32 @@
+package com.salady.saladyJSP;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+public class MyAppContextListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce){
+        // application 시작 시 필요한 초기화 작업 수행
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce){
+
+        // application 종료시 JDBC Connection 해제
+        Enumeration<Driver> drivers = DriverManager.getDrivers();
+        while(drivers.hasMoreElements()) {
+            Driver driver = drivers.nextElement();
+            try {
+                DriverManager.deregisterDriver(driver);
+                System.out.println("Deregistering JDBC driver: " + driver);
+            } catch (Exception e) {
+                System.err.println("Error deregistering JDBC driver: " + driver);
+                e.printStackTrace();
+            }
+        }
+    }
+}
